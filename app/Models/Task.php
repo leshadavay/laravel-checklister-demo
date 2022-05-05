478842b068b71c01f9c42724f1c2267e25f5febb
lesham <leshadavay@gmail.com>
2022-05-05 18:51:42 +0500

    complete user task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -14,7 +14,12 @@ class Task extends Model implements HasMedia
 {
     use HasFactory, SoftDeletes, InteractsWithMedia;
 
-    protected $fillable = ['checklist_id','name','description','position'];
+    protected $fillable = [
+        'checklist_id','name',
+        'description','position',
+        'user_id', 'task_id',
+        'completed_at'
+    ];
 
     public function checklist()
     {
